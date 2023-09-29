718150698fb640891c8fe39cd51f340539002a27
Paul Racisz <paulr@fireflyint.com>
2023-09-29 10:23:30 -0400

    for some reason fetching a file path doesn't work in EXPO go, so may have to import them explicitly

--- components/MainReader.js
+++ components/MainReader.js
@@ -1,17 +1,38 @@
 import React from "react";
-import { ScrollView, Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import BottomNavigation from "./BottomNavigation";
+import GenesisJSONFileContents from "../assets/jsonBibleBooks/Genesis.json";
 
 export default function MainReader() {
+  // import all books, or find another dynamic way that isn't .fetch
+  // by book name as the param, call function and get contents of JSON file
+  // turn JSON file into readable text, and update an HTML DOM component with the text.
+  // create another param for the chapter, and only display the content of that chapter.
+  // ensure that the template is a scrollable view, that allows the user to scroll through the entire chapter.
+  // create arrows in the nav bar, and update the "hello world" text with the chapter name.
+  // the arrow in the nav bar should go to the next or previous chapter. If it is the first chapter go to the previous book. If the book is Genesis...
+  // ...don't render the left arrow.
+  // eventually add the ability to jump to verses not just chapters.
+  // save the chapter and verse so the next time the app opens, it opens on that chapter and verse.
+  // create navigation menu to access each book, chapter and verse of the 66 books of the bible.
   function fetchBibleContent(bookName) {
-    fetch(`../assets/jsonBibleBooks/${bookName}.json`)
-      .then((response) => response.json())
-      .then((data) => {
-        window.alert(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching Bible data:", error);
-      });
+    let stringifiedContent = JSON.stringify(GenesisJSONFileContents);
+    window.alert(stringifiedContent);
+  }
+
+  function urlToBlob(url) {
+    return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+      xhr.onerror = reject;
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          resolve(xhr.response);
+        }
+      };
+      xhr.open("GET", url);
+      xhr.responseType = "blob"; // convert type
+      xhr.send();
+    });
   }
 
   return (
