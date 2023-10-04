12fe01e2d3627bda876796e1f744a77d86a1e338
Paul Racisz <paulieracisz@gmail.com>
2023-10-04 10:39:45 -0400

    FUNCTION

--- components/MainReader.js
+++ components/MainReader.js
@@ -3,87 +3,19 @@ import { Button, ScrollView, Text, StyleSheet, StatusBar } from "react-native";
 import BottomNavigation from "./BottomNavigation";
 import * as kjv from "../assets/assets/bibles/kjv_new.json";
 import * as asv from "../assets/assets/bibles/asv_new.json";
+import * as bookTitles from "../assets/assets/chapterKeys.json";
 
 export default function MainReader() {
-  const bookTitles = {
-    1: "Genesis",
-    2: "Exodus",
-    3: "Leviticus",
-    4: "Numbers",
-    5: "Deuteronomy",
-    6: "Joshua",
-    7: "Judges",
-    8: "Ruth",
-    9: "1 Samuel",
-    10: "2 Samuel",
-    11: "1 Kings",
-    12: "2 Kings",
-    13: "1 Chronicles",
-    14: "2 Chronicles",
-    15: "Ezra",
-    16: "Nehemiah",
-    17: "Esther",
-    18: "Job",
-    19: "Psalms",
-    20: "Proverbs",
-    21: "Ecclesiastes",
-    22: "Song of Solomon",
-    23: "Isaiah",
-    24: "Jeremiah",
-    25: "Lamentations",
-    26: "Ezekiel",
-    27: "Daniel",
-    28: "Hosea",
-    29: "Joel",
-    30: "Amos",
-    31: "Obadiah",
-    32: "Jonah",
-    33: "Micah",
-    34: "Nahum",
-    35: "Habakkuk",
-    36: "Zephaniah",
-    37: "Haggai",
-    38: "Zechariah",
-    39: "Malachi",
-    40: "Matthew",
-    41: "Mark",
-    42: "Luke",
-    43: "John",
-    44: "Acts",
-    45: "Romans",
-    46: "1 Corinthians",
-    47: "2 Corinthians",
-    48: "Galatians",
-    49: "Ephesians",
-    50: "Philippians",
-    51: "Colossians",
-    52: "1 Thessalonians",
-    53: "2 Thessalonians",
-    54: "1 Timothy",
-    55: "2 Timothy",
-    56: "Titus",
-    57: "Philemon",
-    58: "Hebrews",
-    59: "James",
-    60: "1 Peter",
-    61: "2 Peter",
-    62: "1 John",
-    63: "2 John",
-    64: "3 John",
-    65: "Jude",
-    66: "Revelation",
-  };
-  
   const [bookHeader, setBookHeader] = useState(""); // Initialize state for chapter content
   const [chapterContent, setChapterContent] = useState(""); // Initialize state for chapter content
   const [currentTranslation, setCurrentTranslation] = useState(kjv); // Initlialize state for translation, default is KJV
-  const [currentTranslationName, setCurrentTranslationName] = useState("kjv");
+  const [currentTranslationName, setCurrentTranslationName] = useState("KJV");
   const [currentChapterBook, setCurrentChapterBook] = useState(1);
   const [currentChapterState, setCurrentChapterState] = useState(1);
 
   function setTranslation(translation, translationName) {
     setCurrentTranslation(translation);
-    setCurrentTranslationName(translationName);
+    setCurrentTranslationName(translationName.toUpperCase());
     fetchBibleContent(currentChapterBook, currentChapterState);
   }
 
@@ -100,18 +32,27 @@ export default function MainReader() {
     }
 
     // Update the state with the chapter content
-    setBookHeader(bookTitles[bookNumber] + "\n" + chapterNumber);
+    setBookHeader(bookTitles.chapters[bookNumber].title);
     setChapterContent(chapterText);
   }
+
+  function openTranslationModal() {}
 
   return (
     <ScrollView style={readerStyles.scrollView}>
       <Button title="Genesis" onPress={() => fetchBibleContent(1, 1)} />
       <Button title="ASV" onPress={() => setTranslation(asv, "asv")} />
       <Button title="KJV" onPress={() => setTranslation(kjv, "kjv")} />
-      <Text style={readerStyles.header}>{currentTranslationName}</Text>
-      <Text style={readerStyles.header}>{bookHeader}</Text>
-      <Text style={readerStyles.text}>{chapterContent}</Text>
+      <Text
+        onPress={() => openTranslationModal()}
+        style={readerStyles.translation}
+      >
+        {" "}
+        {currentTranslationName}{" "}
+      </Text>
+      <Text style={readerStyles.header}> {bookHeader} </Text>
+      <Text style={readerStyles.chapterNumber}> {currentChapterState} </Text>
+      <Text style={readerStyles.text}> {chapterContent} </Text>
       <BottomNavigation />
     </ScrollView>
   );
@@ -127,11 +68,29 @@ const readerStyles = StyleSheet.create({
     paddingTop: 10,
     paddingTop: StatusBar.currentHeight,
   },
-  header: {
-    fontSize: 55,
-    margin: 15,
+  chapterNumber: {
+    fontSize: 85,
     color: "#fff",
     textAlign: "center",
+  },
+  header: {
+    fontSize: 25,
+    color: "#bababa",
+    fontWeight: "bold",
+    margin: 15,
+    textAlign: "center",
+  },
+  translation: {
+    borderRadius: "500px",
+    backgroundColor: "#404040",
+    top: 0,
+    flex: 1,
+    color: "whitesmoke",
+    fontWeight: "bold",
+    width: "15%",
+    textAlign: "center",
+    fontSize: 15,
+    padding: 5,
   },
   text: {
     fontSize: 25,
